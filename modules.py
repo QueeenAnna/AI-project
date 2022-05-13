878d2bff20b5a06d5a25b4209d6598d96414d3e6
Anna <annatruong96@gmail.com>
2022-05-13 14:58:51 +0200

    Added code on how to load a state dict model on test_4.py and added modles.py

---
+++ modules.py
@@ -0,0 +1,10 @@
+import requests
+import yaml
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import torchvision
+import matplotlib.pyplot as plt
+import seaborn as sn
+import cv2
+import matplotlib as matplotlib
+import pandas
