3133b2e1e89530d3dfc2129439f01422e07ec206
JohannaUlveklint <johanna.ulveklint@gmail.com>
2022-05-19 22:13:07 +0200

    Shows matching ingredients in top recipes

--- extract_labels_and_look_up_recipe.py
+++ extract_labels_and_look_up_recipe.py
@@ -101,16 +101,18 @@ def sort_by_category(recipes):
     return sorted_recipes
 
 
-def show_top_recipes(sorted_recipes, ingredients):
+def show_top_recipes(sorted_recipes):
+    print('These are the top five recipes for you:\n')
     for i in range(5):
-    print('These are the top five recipes for you:\n'
-                       f'1. {sorted_recipes[0]["name"]}\n'
-                       f'2. {sorted_recipes[1]["name"]}\n'
-                       f'3. {sorted_recipes[2]["name"]}\n'
-                       f'4. {sorted_recipes[3]["name"]}\n'
-                       f'5. {sorted_recipes[4]["name"]}\n')
+        ingredient_match = ' '.join(sorted_recipes[i]['ingredients'])
+        print(f'{i+1}. {sorted_recipes[i]["name"]}\n'
+              f'In this recipe you will use: {ingredient_match}\n')
+
     choice = int(input('Which recipe would you like to be directed to? '))
     convert_to_url(sorted_recipes[choice-1]['name'], sorted_recipes[choice-1]['id'])
+
+    # Do we want to ask here if the user has tested the recipe and would like to grade it?
+    # And also give the opportunity to see the alternatives again without having to start from the beginning?
 
 
 def convert_to_url(name, _id):
@@ -130,14 +132,9 @@ def main():
               'olive_label', 'olives', 'onion', 'potato', 'red_onion', 'rhubarb', 'rice', 'salmon', 'spaghetti',  # 18-26
               'spinach', 'sun-dried_tomatoes', 'sun-dried_tomatoes_label', 'tomato_sauce', 'tomato', 'whipping_cream']
     c = get_labels()
-    recipes, ingredients = get_recipes(labels, c)
+    recipes, ingredients = get_recipes(labels, c)  # ingredients may be unnecessary
     sorted_recipes = sort_by_category(recipes)
-    show_top_recipes(sorted_recipes, ingredients)
-
-
-
-
-
+    show_top_recipes(sorted_recipes)
 
     # print(recipes)
 
